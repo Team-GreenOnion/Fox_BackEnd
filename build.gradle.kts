65c7450620d6a34f843327c8ae7c61ff516bfb1c
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-25 09:22:52 +0900

    ⚙️ :: 레디스 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    implementation ("org.springframework.boot:spring-boot-starter-data-redis")
+    annotationProcessor ("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<KotlinCompile> {
