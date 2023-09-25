f18091d1288d6786075d2f0aceb6c1bf7a0852c9
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-25 22:56:26 +0900

    📑 :: User Domain 컬럼 추가

--- src/main/kotlin/com/example/fox_backend/domain/user/domain/User.kt
+++ src/main/kotlin/com/example/fox_backend/domain/user/domain/User.kt
@@ -4,13 +4,12 @@ import com.example.fox_backend.domain.user.enums.Sex
 import com.example.fox_backend.global.entity.BaseEntity
 import java.util.UUID
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
 
 @Entity
 class User  (
     id : UUID,
     val email :String,
+    val accountId : String? = null,
     val password : String,
     val name : String,
     val sex : Sex
