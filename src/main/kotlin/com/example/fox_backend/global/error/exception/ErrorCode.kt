889fc0cf77350be1eb265d6675781f3d65f5cd0c
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-28 02:24:45 +0900

    📑 :: S3 Service 작성

--- src/main/kotlin/com/example/fox_backend/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/fox_backend/global/error/exception/ErrorCode.kt
@@ -17,6 +17,5 @@ enum class ErrorCode (
     USER_NOT_FOUND(404, "User Not Found"),
     NAME_ALREADY_EXISTS(409, "Name Already Exists"),
 
-    IMAGE_UPLOAD_FAILED(400, "Image Upload Failed"),
     IMAGE_VALUE_NOT_FOUND(404, "Image Value Not Found"),
 }
