3365a5f505dbc02b7a51f5003846ba1634261758
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-27 10:46:18 +0900

    📑 :: ErrorCode 추가

--- src/main/kotlin/com/example/fox_backend/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/fox_backend/global/error/exception/ErrorCode.kt
@@ -9,6 +9,10 @@ enum class ErrorCode (
     INVALID_TOKEN(401,"Invalid Token"),
     EXPIRED_TOKEN(401, "Expired Token"),
 
+    EMAIL_CODE_OR_EMAIL_MISS_MATCH(400, "EmailCode Or EMAIL Miss Match"),
+    EMAIL_NOT_FOUND(404, "Email Not Found"),
+
     USER_NOT_FOUND(404, "User Not Found"),
-    PASSWORD_MISS_MATCH(400, "Password Miss Match")
+    PASSWORD_MISS_MATCH(400, "Password Miss Match"),
+    NAME_ALREADY_EXISTS(409, "Nickname Already Exists"),
 }
