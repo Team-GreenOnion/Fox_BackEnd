a28526cd21e3bfb06d430cfb6650f8f1bd93c162
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-27 19:36:28 +0900

    📑 :: entity 수정

--- src/main/kotlin/com/example/fox_backend/domain/user/domain/User.kt
+++ src/main/kotlin/com/example/fox_backend/domain/user/domain/User.kt
@@ -2,15 +2,14 @@ package com.example.fox_backend.domain.user.domain
 
 import com.example.fox_backend.domain.user.enums.Sex
 import com.example.fox_backend.global.entity.BaseEntity
-import java.util.UUID
 import javax.persistence.Entity
 
 @Entity
-class User  (
-    id : UUID,
-    val email :String,
-    val accountId : String? = null,
-    val password : String,
-    val name : String,
-    val sex : Sex
+class User(
+     id : Long?,
+    val email:String,
+    val accountId: String ?= null,
+    val password: String,
+    val name: String,
+    val sex: Sex
 ) : BaseEntity(id)
--- src/main/kotlin/com/example/fox_backend/global/entity/BaseEntity.kt
+++ src/main/kotlin/com/example/fox_backend/global/entity/BaseEntity.kt
@@ -1,20 +1,10 @@
 package com.example.fox_backend.global.entity
 
-import org.hibernate.annotations.GenericGenerator
-import java.util.UUID
-import javax.persistence.Column
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.MappedSuperclass
+import javax.persistence.*
 
 @MappedSuperclass
 abstract class BaseEntity (
     @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid")
-    @Column(
-        columnDefinition = "BINARY(16)",
-        nullable = false
-    )
-    val id: UUID?
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?
 )
