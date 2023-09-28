e925e8c052addffdadb2851e50a09ea2c604e79e
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-28 23:04:03 +0900

    🐛 :: 버그해결

--- src/main/kotlin/com/example/fox_backend/domain/like/domain/Like.kt
+++ src/main/kotlin/com/example/fox_backend/domain/like/domain/Like.kt
@@ -3,12 +3,13 @@ package com.example.fox_backend.domain.like.domain
 import com.example.fox_backend.domain.school.domain.School
 import com.example.fox_backend.domain.user.domain.User
 import com.example.fox_backend.global.entity.BaseEntity
+import javax.persistence.Entity
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 
+@Entity
 class Like (
     id : Long?,
-
     @ManyToOne @JoinColumn(name = "user_id")
     val user : User,
     @ManyToOne @JoinColumn(name = "school_id")
