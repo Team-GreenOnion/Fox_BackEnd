281582fe366d863b1fcc03f168c912e9b8b07bec
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-28 22:00:06 +0900

    📑 :: likeFacade 작성

---
+++ src/main/kotlin/com/example/fox_backend/domain/like/domain/Like.kt
@@ -0,0 +1,16 @@
+package com.example.fox_backend.domain.like.domain
+
+import com.example.fox_backend.domain.school.domain.School
+import com.example.fox_backend.domain.user.domain.User
+import com.example.fox_backend.global.entity.BaseEntity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+
+class Like (
+    id : Long?,
+
+    @ManyToOne @JoinColumn(name = "user_id")
+    val user : User,
+    @ManyToOne @JoinColumn(name = "school_id")
+    val school: School
+) : BaseEntity(id)
