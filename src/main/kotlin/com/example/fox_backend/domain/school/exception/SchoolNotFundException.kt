e55fea0a9ee476f4c9527c7101be787638731118
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-29 16:06:19 +0900

    ⚰️ :: 제거

--- src/main/kotlin/com/example/fox_backend/domain/school/exception/SchoolNotFundException.kt
+++
@@ -1,8 +0,0 @@
-package com.example.fox_backend.domain.school.exception
-
-import com.example.fox_backend.global.error.exception.BusinessException
-import com.example.fox_backend.global.error.exception.ErrorCode
-
-object SchoolNotFundException : BusinessException(
-    ErrorCode.SCHOOL_NOT_FOUND
-)
