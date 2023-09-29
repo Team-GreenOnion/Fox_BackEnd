917cbe0ede77090996d32e7f3abaadbc22e8c4ef
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-29 14:40:25 +0900

    📑 :: school entity 변경

--- src/main/kotlin/com/example/fox_backend/domain/school/domain/School.kt
+++ src/main/kotlin/com/example/fox_backend/domain/school/domain/School.kt
@@ -1,9 +1,15 @@
 package com.example.fox_backend.domain.school.domain
 
+import com.example.fox_backend.domain.school.enums.SchoolDistrict
 import com.example.fox_backend.global.entity.BaseEntity
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 
 @Entity
 class School(
     id: Long?,
+    val schoolName: String,
+    @Enumerated(EnumType.STRING)
+    val schoolDistrict: SchoolDistrict,
 ) : BaseEntity(id)
