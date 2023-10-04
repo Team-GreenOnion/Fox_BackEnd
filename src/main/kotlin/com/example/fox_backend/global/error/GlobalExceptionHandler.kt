13a1e772f68f7fd8113c2dfaa3ed75e44d23f3d3
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-05 08:58:31 +0900

    ⚙️ :: Feign 핸들러 작성

--- src/main/kotlin/com/example/fox_backend/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/fox_backend/global/error/GlobalExceptionHandler.kt
@@ -1,10 +1,13 @@
 package com.example.fox_backend.global.error
 
 import com.example.fox_backend.global.error.exception.BusinessException
+import com.example.fox_backend.global.error.exception.ErrorCode
 import com.example.fox_backend.global.error.exception.ErrorResponse
+import feign.FeignException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import java.time.LocalDateTime
 
@@ -16,5 +19,12 @@ class GlobalExceptionHandler {
         val code = e.errorCode
         val errorResponse = ErrorResponse(code.status, code.message, LocalDateTime.now()) // ErrorResponse 객체 생성
         return ResponseEntity(errorResponse, HttpStatus.valueOf(code.status))
+    }
+
+    @ExceptionHandler(FeignException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun feignExceptionHandler(ex: FeignException): String {
+        ex.printStackTrace()
+        return ex.message ?: ""
     }
 }
