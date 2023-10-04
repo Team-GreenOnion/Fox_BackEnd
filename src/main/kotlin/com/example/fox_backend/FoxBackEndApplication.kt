293ea85bede0adf8bcd996cbfa6e70f28e0067fc
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-04 10:37:17 +0900

    📑 :: Feign 클라이언트 추가

--- src/main/kotlin/com/example/fox_backend/FoxBackEndApplication.kt
+++ src/main/kotlin/com/example/fox_backend/FoxBackEndApplication.kt
@@ -2,10 +2,14 @@ package com.example.fox_backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
-class FoxBackEndApplication
+@EnableFeignClients
+class FoxBackEndApplication {
+}
 
 fun main(args: Array<String>) {
     runApplication<FoxBackEndApplication>(*args)
 }
+
