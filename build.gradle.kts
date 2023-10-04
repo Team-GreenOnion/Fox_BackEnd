41d0c892c49ca7b5b11a03e6563c89908d814d87
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-05 08:57:29 +0900

    ⚙️ :: Feign 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.13"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
-    kotlin("plugin.jpa") version "1.6.21"
+    id("org.springframework.boot") version "2.5.13"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
 group = "com.example"
@@ -24,8 +23,8 @@ configurations {
 repositories {
     mavenCentral()
 }
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
@@ -40,7 +39,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
-    runtimeOnly("com.mysql:mysql-connector-j")
     implementation("mysql:mysql-connector-java:8.0.23")
     implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.21")
@@ -49,13 +47,12 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 }
 
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            freeCompilerArgs += "-Xjsr305=strict"
-            jvmTarget = "17"
-        }
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17" // 여기에서 원하는 JDK 버전을 지정하세요.
     }
+}
 
-    tasks.withType<Test> {
-        useJUnitPlatform()
-    }
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
