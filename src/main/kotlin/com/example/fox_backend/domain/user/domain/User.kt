ca829eb25ea2ac6874d4ad295bb78a5cd4516ce9
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-05 21:57:24 +0900

    📑 :: 양방향 매핑

--- src/main/kotlin/com/example/fox_backend/domain/user/domain/User.kt
+++ src/main/kotlin/com/example/fox_backend/domain/user/domain/User.kt
@@ -1,10 +1,13 @@
 package com.example.fox_backend.domain.user.domain
 
+import com.example.fox_backend.domain.interest.domain.Interest
 import com.example.fox_backend.domain.user.enums.Sex
 import com.example.fox_backend.global.entity.BaseEntity
+import org.apache.catalina.startup.Catalina.ServerXml
 import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
+import javax.persistence.OneToMany
 
 @Entity
 class User(
@@ -14,7 +17,9 @@ class User(
     val name: String,
     var profileUrl: String? = null,
     @Enumerated(EnumType.STRING)
-    val sex: Sex
+    val sex: Sex,
+    @OneToMany
+    val interest: List<Interest>? = null
 ) : BaseEntity(id) {
     fun modifyPassword(password: String) {
         this.password = password
