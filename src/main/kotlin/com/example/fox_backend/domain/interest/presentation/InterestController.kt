8e97cf245b70580fdfe61c3a2f20026bd0dd1dd4
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-06 09:54:28 +0900

    ⚡️ :: 관심분야 수정 API

--- src/main/kotlin/com/example/fox_backend/domain/interest/presentation/InterestController.kt
+++ src/main/kotlin/com/example/fox_backend/domain/interest/presentation/InterestController.kt
@@ -2,6 +2,8 @@ package com.example.fox_backend.domain.interest.presentation
 
 import com.example.fox_backend.domain.interest.presentation.dto.request.InterestRequest
 import com.example.fox_backend.domain.interest.service.InterestService
+import com.example.fox_backend.domain.interest.service.ModifyInterestService
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/interest")
 class InterestController(
-    private val interestService: InterestService
+    private val interestService: InterestService,
+    private val modifyInterestService : ModifyInterestService
 ) {
     @PostMapping
     fun selectInterest(interestRequest: InterestRequest) =
         interestService.selectInterest(interestRequest)
+
+    @PatchMapping
+    fun modifyInterest(interestRequest: InterestRequest) =
+         modifyInterestService.modifyInterest(interestRequest)
+
 }
